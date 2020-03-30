5e2d7944034a1d7f66a4f323f3ff960d4f202097
宮崎貴史 <>
2020-03-30 10:50:03 +0900

    add sign and verify

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express = require("express")
-const app = express()
+const jwt = require('jsonwebtoken')
+const getToken = require('./token')
 
+const app = express()
 const server = app.listen(3333, () => {
   console.log("Node.js is listening to PORT:" + server.address().port)
 })
 
-app.get("/api/test", (req, res, next) => {
+const secretKey = 'kkofjow78310ifjl'
+
+app.get('/api/sign', (req, res) => {
+  const user = {
+    id: 101,
+    name: 'miyazaki'
+  }
+
+  const token = jwt.sign(user, secretKey, { expiresIn: '30s' })
+
   res.json({
-    id: 100,
-    messages: 'test response zzzz'
+    token
   })
+})
+
+
+app.get('/api/post', (req, res, next) => {
+  const token = getToken(req)
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if(err !== null){
+      res.json({
+        err,
+        token
+      })
+    }
+    res.json({
+      result: 'post ok',
+      user: decoded,
+    })
+  })
+
 })
